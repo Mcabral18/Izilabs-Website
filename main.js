141ab5c362605d0083ed5600bd03910d5938efd2
Marco Cabral <cabralovil@gmail.com>
2020-06-15 16:38:42 +0100

    update

---
+++ main.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            setting: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 520,
+            setting: {
+                slidesToShow: 1
+            }
+        }]
+    });
+});
+
+/*
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+  
+    $('html, body').animate(
+      {
+        scrollTop: images
+      },
+      900
+    );
+  });
+  */
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
